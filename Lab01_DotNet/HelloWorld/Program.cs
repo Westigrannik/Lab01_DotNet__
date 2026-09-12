cdb48ffd4784ccab07235129ac1b582a7c7bba46
Westigrannik <timvovk5th@gmail.com>
2026-09-12 11:04:14 +0300

    test

---
+++ Lab01_DotNet/HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// int age = 20;
+// double gpa = 4.5;
+// bool stud = true;
+// string name = "Ivan";
+
+
+
+// Console.WriteLine(stud);
+// Console.WriteLine(age);
+// Console.WriteLine(gpa);
+// Console.WriteLine(name);
+
+string first = "Вовк";
+string last = "Тимофей";
+string group = "ИСП-242";
+int birthYear = 2007;
+double gpa = 4.5;
+bool hasScholarship = true;
+int currentYear = 2026;
+int age = currentYear - birthYear;
+
+Console.WriteLine("Студенческое удостоверение");
+Console.WriteLine($"Имя: {first} {last}");
+Console.WriteLine($"Группа: {group}");
+Console.WriteLine($"Возраст: {age} лет");
+Console.WriteLine($"Средний бал: {gpa}");
+Console.WriteLine($"Стипендия: {hasScholarship}");
+
+Console.Write("\nВведите ваш любимый предмет: ");
+string subject = Console.ReadLine();
+Console.WriteLine($"Отлично! {first} любит {subject}");
